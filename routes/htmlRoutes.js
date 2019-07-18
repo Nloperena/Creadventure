db3ac2931c259a2fd6b3e4551214b16c55fa5aff
Delane Chen <delane4.0@gmail.com>
2019-07-18 11:07:32 -0400

    handlebars and I are seeing different people

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,16 +20,19 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/test",function(req,res){
-    res.sendFile(path.join(__dirname, "../public/signupTest.html"));
+  app.get("/account",function(req,res){
+    res.render("account")
   })
 
   app.get("/login/:user", function(req, res) {
-    db.userbase.findOne({ where: { username: req.params.user } }).then(function(user) {
-      //console.log(user)
-      res.render("account", {
-        user: user
-      });
+    db.userbase.findOne({ where: { username: req.params.user } }).then(function(data) {
+      console.log("User in route ", data)
+
+      var obj = {
+        userUser: data
+      }
+      res.render("account", obj);
+      
     });
   });
 
